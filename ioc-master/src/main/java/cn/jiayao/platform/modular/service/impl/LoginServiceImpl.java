b54131b8c30e849adff3e4637e69d3c4e900ae72
smile960207 <76508578+smile960207@users.noreply.github.com>
2021-03-03 19:09:54 +0800

    Update LoginServiceImpl.java

--- ioc-master/src/main/java/cn/jiayao/platform/modular/service/impl/LoginServiceImpl.java
+++ ioc-master/src/main/java/cn/jiayao/platform/modular/service/impl/LoginServiceImpl.java
@@ -5,15 +5,6 @@ import cn.jiayao.platform.core.annotation.MyService;
 import cn.jiayao.platform.modular.dao.LoginMapping;
 import cn.jiayao.platform.modular.service.LoginService;
 
-/**
- * 类 名: LoginServiceImpl
- * 描 述:
- * 作 者: 黄加耀
- * 创 建: 2019/3/17 : 9:15
- * 邮 箱: huangjy19940202@gmail.com
- *
- * @author: jiaYao
- */
 @MyService(value = "loginServiceImplTest")
 public class LoginServiceImpl implements LoginService {
 
